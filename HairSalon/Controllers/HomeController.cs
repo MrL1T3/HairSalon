a897c652ba955061043122230abdc5778773a871
Emil Anton <emilbstn@gmail.com>
2023-05-24 13:51:44 -0700

    change namespace to HairSalon

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace VendorOrderTracker.Controllers
+namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace VendorOrderTracker
+namespace HairSalon
 {
   class Program
   {
